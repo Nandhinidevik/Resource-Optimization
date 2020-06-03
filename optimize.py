297dd6f8c85cb192fca912c5d36212e16812fbde
Nandhinidevik <57682903+Nandhinidevik@users.noreply.github.com>
2020-06-04 00:11:22 +0530

    Update optimize.py

--- optimize.py
+++ optimize.py
@@ -1,17 +1,23 @@
+#Data provided to estimate the optimized result
 Units = {"Large" : 10, "XLarge" : 20, "2XLarge" : 40 , "4XLarge" : 80, "8XLarge" : 160 , "10XLarge" : 320}
 NewYork = {"Large" : 120, "XLarge" : 230, "2XLarge" :450 ,"4XLarge":774 , "8XLarge" : 1400, "10XLarge" : 2820 }
 India = {"Large" : 140, "XLarge" : "null", "2XLarge" :413 ,"4XLarge":890 , "8XLarge" : 1300, "10XLarge" : 2970 }
 China = {"Large" : 110, "XLarge" : 200, "2XLarge" :"null" ,"4XLarge":670 , "8XLarge" : 1180, "10XLarge" : "null" }
 
+#Global variables
 ActualCost = 0
 TempMachineList = []
 MachineList = []
+
+#SampleInput : 100 units for 5 hours
 Input = input()
 InputList = Input.split() 
 
+#Given the required unit, this function returns the list of all the machines capable to work on the given unit.
 def get_list(Value):
     return [d for d in Units.keys()  if Units[d] <= Value]
-    
+ 
+#Returns the number and cost of particular machine and the remaining units to be processed by other smaller machines.
 def get_cost(cost, AvailableUnit, RequiredUnit, Country, Hour):
     Value = RequiredUnit // Units[AvailableUnit]
     NewCost = cost + (Country[AvailableUnit] * Value * Hour)
