3b244050ba95adbf01c1e07af7a50fbaea1e1555
Nandhinidevik <57682903+Nandhinidevik@users.noreply.github.com>
2020-06-03 23:58:00 +0530

    Create optimize.py

---
+++ optimize.py
@@ -0,0 +1,52 @@
+Units = {"Large" : 10, "XLarge" : 20, "2XLarge" : 40 , "4XLarge" : 80, "8XLarge" : 160 , "10XLarge" : 320}
+NewYork = {"Large" : 120, "XLarge" : 230, "2XLarge" :450 ,"4XLarge":774 , "8XLarge" : 1400, "10XLarge" : 2820 }
+India = {"Large" : 140, "XLarge" : "null", "2XLarge" :413 ,"4XLarge":890 , "8XLarge" : 1300, "10XLarge" : 2970 }
+China = {"Large" : 110, "XLarge" : 200, "2XLarge" :"null" ,"4XLarge":670 , "8XLarge" : 1180, "10XLarge" : "null" }
+
+ActualCost = 0
+TempMachineList = []
+MachineList = []
+Input = input()
+InputList = Input.split() 
+
+def get_list(Value):
+    return [d for d in Units.keys()  if Units[d] <= Value]
+    
+def get_cost(cost, AvailableUnit, RequiredUnit, Country, Hour):
+    Value = RequiredUnit // Units[AvailableUnit]
+    NewCost = cost + (Country[AvailableUnit] * Value * Hour)
+    rem = RequiredUnit % Units[AvailableUnit]
+    return NewCost, rem, Value
+    
+def process(cost, ActCost, Units, Country, Hour):
+    global ActualCost
+    global TempMachineList
+    global MachineList
+    ActualCost = ActCost
+    List = get_list(Units)
+    for i in List:
+      if Country[i] != "null":
+          NewCost, rem, Value= get_cost(cost, i, Units, Country, Hour)
+          if rem == 0:
+            if NewCost < ActualCost or ActualCost == 0:
+                ActualCost = NewCost
+                TempMachineList.append((i, Value))
+                MachineList = TempMachineList
+                TempMachineList = []
+            else:
+                TempMachineList = []
+          else:
+           TempMachineList.append((i, Value))
+           process(NewCost, ActualCost, rem, Country, Hour)
+
+if len(InputList) == 5: 
+    [TotalUnits, _, _, Hour, _] = InputList
+    process(0,0,int(TotalUnits), NewYork, int(Hour))
+    NewYorkRes = {"region":"New York","total_cost":"$"+str(ActualCost),"machines": MachineList}
+    process(0,0,int(TotalUnits), India, int(Hour))
+    IndiaRes = {"region":"India","total_cost":"$"+str(ActualCost),"machines": MachineList}
+    process(0,0,int(TotalUnits), China, int(Hour))
+    ChinaRes = {"region":"China","total_cost":"$"+str(ActualCost),"machines": MachineList}
+    print({"Output":[NewYorkRes, IndiaRes, ChinaRes]})
+else:
+    print("Incorrect Input format")
